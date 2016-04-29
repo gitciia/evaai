4b7ce8614f556743be7b765153128536c1d58a2f
rompehielos <system00082@gmail.com>
2016-04-29 15:53:44 -0500

    Update class-ai-db.php

--- ai-adjuntos/class-ai-db.php
+++ ai-adjuntos/class-ai-db.php
@@ -5,7 +5,7 @@ class Conexion extends mysqli {
 
 
 	public function __construct(){
-		parent:: __construct('localhost','root','1turing1','dblearning');
+		parent:: __construct('localhost','root','0turing0','dblearning');
 		#	$this->query("SET NAMES'utf8';");
 			#$this->connect_errno ? die('error en la Conexion');
 	}
